d3355cd4085cc3ddddbc9970415ccc4f86e43622
Eric Lee <ericjlee@hotmail.com>
2019-03-22 21:09:29 -0400

    add templates

--- lunch_and_learn/main/urls.py
+++ lunch_and_learn/main/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from . import views
+
+app_name = 'main'
+
 urlpatterns = [
-    path('api/user/', views.UserList.as_view())
+    #path('api/user/', views.UserList.as_view())
+    path('api/user/', views.UserList.as_view()),
+    path("", views.homepage, name="homepage"),
+    path("register/", views.register, name="register"),
+    path("logout", views.logout_request, name="logout"),
+    path("login", views.login_request, name="login")
 ]
