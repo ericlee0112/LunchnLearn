3a3e8d3ee3423664c824cbc43c81dc188f28e3c0
akandadai <anantkandadai@gmail.com>
2019-03-31 08:31:00 -0400

    commented more for clean up

--- lunch_and_learn/main/models.py
+++ lunch_and_learn/main/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     username = models.CharField(primary_key = True, max_length=100)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    password = models.CharField(max_length=20)
+    # password = models.CharField(max_length=20)
 
 class Skill(models.Model):
     skill_name = models.CharField(max_length = 100, primary_key = True)
